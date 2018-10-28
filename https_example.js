b391efdbdeb19214b80b34e2a2ae257e955a51d5
Andy Lindsay <andydlindsay@gmail.com>
2018-10-28 23:21:42 +0000

    added simple https request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,18 @@
 var https = require('https');
+var host = 'www.example.org';
+var log = console.log;
+var options = {
+  host,
+  path: '/'
+};
 
-console.log('I did it!');
+// called by https when the request is made
+var callback = function() {
+  log('In response handler callback!');
+};
+
+log('I\'m about to make the request!');
+
+https.request(options, callback).end();
+
+log('I\'ve made the request!');
